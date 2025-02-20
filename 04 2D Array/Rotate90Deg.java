073dafd654b962809f5079be8660b2f5bda3c235
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-02-21 01:57:28 +0530

    Rotate 2D array by 90 degree

---
+++ 04 2D Array/Rotate90Deg.java
@@ -0,0 +1,31 @@
+
+public class Rotate90Deg {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<i; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            int start = 0;
+            int end = arr.length-1;
+            while(start < end){
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
