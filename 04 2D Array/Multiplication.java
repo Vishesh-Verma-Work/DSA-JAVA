e177653e23ab11e1d84ec951de7b3d48293cbe4f
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-02-26 00:37:04 +0530

    Mul of 2D mat

---
+++ 04 2D Array/Multiplication.java
@@ -0,0 +1,23 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+        int[][] a = {{1,2,3},{4,5,6}};
+        int[][] b = {{1,2},{3,4},{5,6}};
+        int[][] c = new int[a.length][b[0].length];
+
+        for(int i=0; i<c.length; i++){
+            for(int j=0; j<c[0].length; j++){
+                for(int k=0; k<a[0].length; k++){ // k<b.length();
+                    c[i][j] += a[i][k]*b[k][j];
+                }
+            }
+        }
+
+        for(int i=0; i<c.length;i++){
+            for(int j=0; j<c[0].length; j++){
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
