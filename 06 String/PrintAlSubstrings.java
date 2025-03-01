5f501fcec49a54ec966e941b40aabd26ee804833
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-01 17:07:02 +0530

    Print all substrings of an string

---
+++ 06 String/PrintAlSubstrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintAlSubstrings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        for(int i=0; i<str.length(); i++){
+            for(int j=i+1; j<str.length()+1; j++){
+                System.out.print(str.substring(i, j) + " ");
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
