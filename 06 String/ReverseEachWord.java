1d0b5b5a6b1a7b585f87148b1613e82aafb656f7
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-06 10:52:48 +0530

    reverse word

--- 06 String/ReverseEachWord.java
+++ 06 String/ReverseEachWord.java
@@ -1,16 +1,36 @@
 public class ReverseEachWord {
+    public static void rev(int start,int end, StringBuilder sb){
+        while(start < end){
+            char ch = sb.charAt(start);
+            sb.setCharAt(sb.charAt(start) , sb.charAt(end));
+            sb.setCharAt(sb.charAt(end) , ch);
+            start++;
+            end--;
+        }
+    }   
     public static void main(String[] args) {
         // i am vishesh = i ma hsehsiv
-        StringBuilder sb = new StringBuilder("i am vishesh  verma");
-        StringBuilder ans = new StringBuilder(sb.length());
+        StringBuilder sb = new StringBuilder("vishesh verma ");
+        // StringBuilder ans = new StringBuilder(sb.length());
         // ascii of space = 32
+        // for(int i=0; i<sb.length(); i++){
+        //     if((int)(sb.charAt(i)) == 32){
+        //         ans.append(" ");
+        //     }else{
+        //         // fill the code 
+        //         // else code
+        //     }
+        // }
+
+        int start = 0, end = 0;
         for(int i=0; i<sb.length(); i++){
-            if((int)(sb.charAt(i)) == 32){
-                ans.append(" ");
-            }else{
-                // fill the code 
-                // else code
-            }
+        if((int)sb.charAt(end+1) == 32 || end == sb.length()-1){
+            // call the function
+            rev(start,end, sb);
+            start = end;
+        }else end++;
         }
+
+        
     }
 }
