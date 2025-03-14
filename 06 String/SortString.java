60e3e6d431fcbf5d959c5107420e64b4c9e94537
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-14 18:02:48 +0530

    C

--- 06 String/SortString.java
+++ 06 String/SortString.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class SortString {
+    // sort the string by conversion
     public static void main(String[] args) {
         String str  = "vishesh";
         StringBuilder sb = new StringBuilder("verma");
