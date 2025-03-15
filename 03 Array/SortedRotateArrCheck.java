fd438aec600f738810aea7c4f53cb3cff3e4455b
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-15 20:18:19 +0530

    Check arr is sorted after one roatation by n place

---
+++ 03 Array/SortedRotateArrCheck.java
@@ -0,0 +1,23 @@
+public class SortedRotateArrCheck {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int i = 0;  
+     int newNum = 0;
+     for(i=0; i<arr.length-1; i++){
+        if(arr[i] <= arr[i+1]) continue;
+        else{
+            newNum = i+1;
+            break;
+        }
+    }
+    if(newNum == 0) System.out.println("t");
+    for(int j=newNum; j<arr.length-1; j++){
+        if(arr[j] <= arr[j+1]) continue;
+        else System.out.println("f");
+    }   
+    if(arr[arr.length-1] <= arr[0]) System.out.println("t");
+    //fix a bit , just use a flag
+     System.out.println("f");
+    
+    }
+}
