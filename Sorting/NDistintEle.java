e5233ca83d264b1d02f24c9628d53e5aea9c44b2
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-20 22:09:45 +0530

    Transforming the array in sorted manner of n strating by 0

---
+++ Sorting/NDistintEle.java
@@ -0,0 +1,42 @@
+
+// public class NDistintEle {
+//     public static void main(String[] args) {
+//         int[] arr = {54,11,28,47,91,63};
+//         int[] temp = Arrays.copyOf(arr, arr.length);
+//         Arrays.sort(temp);
+//         int x = -1;
+//         for(int i=0; i<temp.length; i++){
+//             for(int j=0; j<temp.length; j++){
+//                 if(temp[i] == arr[j]) arr[j] = ++x;
+//             }
+//         }
+//         for(int ele : arr){
+//             System.out.print(ele + " ");
+//         }
+//     }
+// }
+
+
+public class NDistintEle {
+    public static void main(String[] args) {
+        int[] arr = {54,11,28,47,91,63};
+        int x = 0, idx = -1;
+        for(int i=0; i<arr.length; i++){
+            int min = Integer.MAX_VALUE;
+            idx = -1;
+            for(int j=0; j<arr.length; j++){
+            if(arr[j] < min && arr[j] > x){
+                min = arr[j];
+                idx = j;
+            }
+        }
+        if (idx != -1) {
+            arr[idx] = x++;
+        }
+        }   
+
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
