8f6e49a8d31aa05dfdf6fc95f653090f4d2ceeda
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-03-26 09:44:37 +0530

    Binary Seaching

---
+++ 08 Binary Search/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,3,5,9,12};
+        int target = 19;
+        int lw = 0, up = arr.length-1;
+        while(lw <= up){
+            int mid = (lw+up)/2;
+            if(arr[mid] == target){
+                System.out.println(mid);
+                return;
+            }
+            else if(arr[mid] < target) lw = mid+1;
+            else up = mid-1;
+        }
+        System.out.println("not found");
+    }
+}
