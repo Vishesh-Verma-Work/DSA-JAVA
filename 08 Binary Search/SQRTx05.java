d3f06febf7e43fd3b2268835778cacc9d450f25d
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-04-02 10:38:56 +0530

    Description of the question

--- 08 Binary Search/SQRTx05.java
+++ 08 Binary Search/SQRTx05.java
@@ -1,7 +1,8 @@
+// this is the description of the question
 // Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 // You must not use any built-in exponent function or operator.
 // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
- 
+
 // Example 1:
 // Input: x = 4
 // Output: 2
