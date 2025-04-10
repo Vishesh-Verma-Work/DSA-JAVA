38f9720e066345f112be6d5cfe63529653c4043c
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-04-10 20:20:41 +0530

    code added R

--- 08 Binary Search/SmallestDivThreshold.java
+++ 08 Binary Search/SmallestDivThreshold.java
@@ -46,33 +46,3 @@ public class SmallestDivThreshold {
         // desription done
     }
 }
-
-// class Solution {
-//     public int smallestDivisor(int[] arr, int t) {
-//         int mx = -1;
-//         for(int i=0; i<arr.length; i++){
-//         //mx = Math.max(mx,arr[i]);
-//             if(mx < arr[i]){
-//                 mx = arr[i];
-//             }
-//         }
-//         // i get the max value
-//         int lw = 1; 
-//         int up = mx;
-//         int sum = 0;
-//         int mid = 1;;
-//         while(lw<=up){
-//             mid = lw + (up-lw)/2;
-//             sum = 0;
-//             for(int i=0; i<arr.length; i++){
-//                 if(arr[i]%mid==0) sum += arr[i]/mid;
-//                 else sum += arr[i]/mid+1;
-//             }
-//             if(sum <= t){
-//                 up = mid-1; 
-//             }
-//             else lw = mid+1;
-//         }
-//         return lw;
-//     }
-}
