6d4480f1ae529f8792d78e11b43f8935c58af0c6
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-05-19 15:37:41 +0530

    Maze path question solved

--- 09 Recursion/mazePath.java
+++ 09 Recursion/mazePath.java
@@ -1,5 +1,11 @@
 public class mazePath {
+    public static int paths(int row,int col){
+        if(row == 1 || col == 1) return 1;
+        int down = paths(row, col-1);
+        int right =  paths(row-1, col);
+        return down+right;
+    }
     public static void main(String[] args) {
-        
+     System.out.println(paths(3,4));   
     }
 }
